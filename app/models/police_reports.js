d1b4893d07e18bf2c96e629b184ea008d3ada4a6
potatofrays <raynnanna@gmail.com>
2018-05-01 16:38:36 +0800

    Updated

--- app/models/police_reports.js
+++ app/models/police_reports.js
@@ -25,7 +25,7 @@ var vehicleSchema = new Schema({
 var policeReport = new Schema({
 	committed_at: {type: Date},
 	citizen_reported_at:{type: Date},
-	reported_at: {type: Date},
+	reported_at: {type: Date, default: Date.now},
 	accident_type: {type: String},
 	accident_cause: {type: String},
 	police_username:{type: String},
