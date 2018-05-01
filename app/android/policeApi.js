3471d522ff6d26b924c29200209b4cdfcd101a1f
potatofrays <raynnanna@gmail.com>
2018-05-01 16:11:09 +0800

    latest

--- app/android/policeApi.js
+++ app/android/policeApi.js
@@ -8,7 +8,7 @@ module.exports = function(router){
 		Police_User.findOne({police_username: req.params.username},{police_password: req.params.password}, function(err, user){
 			if (err) {
 		            res.json({ success: false, message: err });
-			}else{
+			} else {
 				if (!user) {
 		            res.json({ success: false, message: 'Username not found' }); // Username not found in database
 		        } else if (user) {
@@ -20,8 +20,12 @@ module.exports = function(router){
 					        if (!validPassword) {
 					            res.json({ success: false, message: 'Could not authenticate password' }); // Password does not match password in database
 					        } else {
-					        	Police_User.findOne({police_username: req.params.username}, function(err, username){
-					          		return res.json({ success: true, username: username.police_username, police_id: username.id, police_station: username.police_station}); // Return token in JSON object to controller
+										Police_User.findOne({police_username: req.params.username},{ police_permission: 'user'}, function(err, username){
+											if (username.police_permission !== 'user'){
+												 res.json({ success: false, message: 'You must be a police user to log in' }); // Password was not provided
+											} else {
+												return res.json({ success: true, username: username.police_username, police_id: username.id, police_station: username.police_station}); // Return token in JSON object to controller
+											}
 					       		});
 					        }
 					}
