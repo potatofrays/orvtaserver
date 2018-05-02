0501a08a2697f4c6e8125db3d99f574dc75b2888
potatofrays <raynnanna@gmail.com>
2018-05-02 16:45:06 +0800

    LATEST

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-angular.module('userApp', ['appRoutes', 'userControllers', 'userServices', 'ngAnimate', 'mainController', 'authServices', 'resetController', 'managementController','reportServices','reportManagementController', 'addCtrl','headerCtrl', 'geolocation', 'gservice', 'resetRequestController'])
+angular.module('userApp', ['appRoutes', 'userControllers', 'userServices', 'ngAnimate', 'mainController', 'authServices', 'resetController', 'managementController','reportServices','reportManagementController', 'addCtrl','headerCtrl', 'geolocation', 'gservice', 'resetRequestController','angularjs-datetime-picker'])
 
 .config(function($httpProvider) {
     $httpProvider.interceptors.push('AuthInterceptors');
